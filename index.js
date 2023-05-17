d853b859b0d902e6f2bafffdfec3740f9d919d32
Sagar Barate <sagarbarate2907@com.com>
2023-05-17 11:20:49 +0530

    layouts

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 //use express router
+app.use('/',require('./routes'));
 
+//set up of the view engine
 app.set('view engine', "ejs")
-
 app.set('views', './views');
 
 
-app.use('/',require('./routes'));
 
 app.listen(port, function(err){
     if (err){
