542d5cec8af558acfae29e46a65ba3cd5c8b9b6e
Sagar Barate <sagarbarate2907@com.com>
2023-05-17 11:55:54 +0530

    setup static files for layouts and pages

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');  //requireing the layouts 
 
+//Layouts
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+//extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/',require('./routes'));
